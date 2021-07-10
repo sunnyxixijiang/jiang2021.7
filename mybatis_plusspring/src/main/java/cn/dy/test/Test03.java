5f917e6d65e77ac2b1f6d5af7fa6f405d15ea3dd
jianghaixv <1935600075@qq.com>
2021-07-10 08:19:08 +0800

    Mac版本git第一个推送文件

---
+++ mybatis_plusspring/src/main/java/cn/dy/test/Test03.java
@@ -0,0 +1,11 @@
+package cn.dy.test;
+
+@SuppressWarnings("all")
+
+// Jiang 2021/7/10
+
+public class Test03 {
+    public static void main(String[] args) {
+        System.out.println("Mac版本第一个推送类");
+    }
+}
